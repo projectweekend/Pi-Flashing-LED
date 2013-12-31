5821ea91caa960f54b0682e42d600f5662760e70
Brian Hines <brian@projectweekend.net>
2013-12-31 00:32:57 -0600

    Wire up a couple of new routes

---
+++ leds.py
@@ -0,0 +1,34 @@
+import time
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GREEN_LED = 18
+RED_LED = 23
+GPIO.setup(GREEN_LED, GPIO.OUT)
+GPIO.setup(RED_LED, GPIO.OUT)
+
+
+def flash_led(led_to_flash):
+    
+    i = 0
+    while i <= 10:
+        
+        if i % 2 == 0:
+            GPIO.output(led_to_flash, True)
+        else:
+            GPIO.output(led_to_flash, False)
+
+        i += 1
+        time.sleep(1)
+
+    GPIO.output(led_to_flash, False)
+
+
+def flash_green():
+    flash_led(GREEN_LED)
+
+
+def flash_red():
+    flash_led(RED_LED)
--- server.py
+++ server.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
 
+import leds
+
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def main_route():
     return render_template('main.html')
+
+
+@app.route('/flash/red', methods=['POST'])
+def red_route():
+    leds.flash_red()
+
+
+@app.route('/flash/green', methods=['POST'])
+def green_route():
+    leds.flash_green()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
