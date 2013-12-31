3318f4360d1dee359629516d7c0dd13af935b41b
Brian Hines <brian@projectweekend.net>
2013-12-30 23:17:48 -0600

    Add main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GREEN_LED = 18
+RED_LED = 23
+GPIO.setup(GREEN_LED, GPIO.OUT)
+GPIO.setup(RED_LED, GPIO.OUT)
+
+i = 0
+
+while i <= 10:
+    if i % 2 == 0:
+        GPIO.output(GREEN_LED, True)
+        GPIO.output(RED_LED, False)
+    else:
+        GPIO.output(GREEN_LED, False)
+        GPIO.output(RED_LED, True)
+    i += 1
+    time.sleep(2)
